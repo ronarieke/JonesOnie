4b97f13f5b91820873a9626211d333479e2f303a
Alex Rieke <ronarieke@gmail.com>
2019-12-15 15:53:37 -0600

    Add files via upload

---
+++ StockPredictionRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimbaForceLibrary.Models
+{
+    public class StockPredictionRecord
+    {
+        public string id { get; set; }
+        public int recordId { get; set; }
+        public DateTime entryTime { get; set; }
+        public string recordType { get; set; }
+        public string record { get; set; }
+    }
+}
